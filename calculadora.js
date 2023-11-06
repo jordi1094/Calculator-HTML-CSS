6ed60cfa5370d611c8f5f6274b26d64ebc7993e8
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2023-11-06 21:48:31 +0100

    finalizado sin porcentajes
    falta pensar como poner calculo de porcenttajes

--- calculadora.js
+++ calculadora.js
@@ -1,10 +1,30 @@
-const resultado = document.querySelector(".resultado");
+const formula = document.querySelector(".formula");
+const resultado = document.querySelector(".resultado")
 const botones = document.querySelectorAll(".boton");
 
 botones.forEach(boton =>{
     boton.addEventListener("click",() => {
         const botonApretado = boton.textContent;
+        if ( boton.className === "AC boton") {
+            formula.textContent = "";
+            resultado.textContent = "0";
+            return;
+        }
 
-        resultado.textContent = botonApretado;
+        if (boton.className === "operando boton") {
+            formula.textContent = eval(formula.textContent);
+        }
+
+        if (boton.className === "igual boton") {
+            resultado.textContent = eval(formula.textContent);
+            
+            formula.textContent = ""
+        }
+
+        if (formula.textContent === "" || formula.textContent === "=") {
+            formula.textContent = botonApretado;
+        }else{
+        formula.textContent += botonApretado;
+        }
     })
 })
