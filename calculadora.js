25e504457069100258772b18de0c5ec313e29fb5
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2023-11-06 21:06:00 +0100

    upload
    sigue sin funcionar el js

--- calculadora.js
+++ calculadora.js
@@ -1,10 +1,10 @@
-const pantalla = document.querySelector(".resultado");
+const resultado = document.querySelector(".resultado");
 const botones = document.querySelectorAll(".boton");
 
 botones.forEach(boton =>{
     boton.addEventListener("click",() => {
         const botonApretado = boton.textContent;
 
-        resulttado.textContent = botonApretado
+        resultado.textContent = botonApretado;
     })
 })
